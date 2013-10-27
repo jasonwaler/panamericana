e23548d2c58198921a84133715a825ba248354af
Joel <jason@github.leemail.me>
2013-10-27 14:45:32 +0000

    Added homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Tweeter::Application.routes.draw do
   devise_for :users
   resources :tweets
 
+  root to: 'home#show'
+  
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
