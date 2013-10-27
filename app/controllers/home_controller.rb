3e6630991a0972a6050074320fa14689fc89d3df
Joel <jason@github.leemail.me>
2013-10-27 15:08:08 +0000

    Reverse tweet order

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,8 @@ class HomeController < ApplicationController
   skip_before_filter :authenticate_user!
   
   def show
-    @tweets = Tweet.all
+    @tweets = Tweet.all.order('created_at DESC')
+    #the created_at DESC means the order of tweets will be returned in descending order
   end
 
 end
